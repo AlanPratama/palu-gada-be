caaafc7f1cb339ecda10c292f3cbb80b9fb17c61
AlanPratama <pralan76@gmail.com>
2024-09-11 19:21:15 +0700

    initial commit

---
+++ src/main/java/com/palu_gada_be/palu_gada_be/PaluGadaBeApplication.java
@@ -0,0 +1,13 @@
+package com.palu_gada_be.palu_gada_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaluGadaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaluGadaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palu_gada_be/palu_gada_be/PaluGadaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palu_gada_be.palu_gada_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaluGadaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
